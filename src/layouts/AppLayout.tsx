ced8946605b66371247461d90c17017bad6c6560
andry_halo2@hotmail.com <andry_halo2@hotmail.com>
2025-12-30 06:24:50 -0600

    fix: Remove spacer overlap on workout pages to show all buttons

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -148,7 +148,7 @@ export const AppLayout = () => {
             <main className="flex-1 overflow-y-auto custom-scrollbar relative flex flex-col">
                 <Outlet />
                 {/* Spacer to prevent BottomNav overlap (Only when BottomNav is visible) */}
-                {shouldShowBottomNav && !isReelsPage && <div className="h-24 shrink-0" />}
+                {shouldShowBottomNav && <div className="h-24 shrink-0" />}
             </main>
 
             {/* Global Modals */}
